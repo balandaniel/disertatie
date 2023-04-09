19b2404d2bd55ac00795fd4fc7d5c8ee49cdd82f
Balan Daniel <ballan.daniel@yahoo.com>
2023-04-09 22:58:39 +0300

    Small changes for functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -49,6 +49,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     # Test method
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Daniel has heard about a cool new online to-do app.
@@ -72,35 +77,21 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Learn about how TDD works')
 
         # When he hits enter, the page updates, and now the page lists
-        # "1. Buy peacock feathers" as an item in a to-do list
+        # "1: Learn about how TDD works" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        #self.assertTrue(
-            #any(row.text == '1: Learn about how TDD works' for row in rows),
-            #f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        #)
-
-        self.assertIn('1: Learn about how TDD works', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Learn about how TDD works')
 
         # There is still a text box inviting her to add another item. He enters
-        # "Use peacock feathers to make a fly"
+        # "Use TDD knowledge to write masters degree thesis"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use TDD knowledge to write masters degree thesis')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
 
         # The page updates again, and now shows both items on his list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Learn about how TDD works', [row.text for row in rows])
-        self.assertIn(
-            '2: Use TDD knowledge to write masters degree thesis',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Learn about how TDD works')
+        self.check_for_row_in_list_table('2: Use TDD knowledge to write masters degree thessis')
 
         # Daniel wonders whether the site will remember his list. Then he sees
         # that the site has generated n unique URL for his -- there is some
